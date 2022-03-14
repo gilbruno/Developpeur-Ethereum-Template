ea33b60743c59bb6bdafb817e1eb5d7d979e152d
Gilles Bruno <gilles.bruno@bappli.com>
2022-03-14 12:10:57 +0100

    new functions declaration

--- 3. Solidity/Whitelist.sol
+++ 3. Solidity/Whitelist.sol
@@ -12,8 +12,19 @@ contract Whitelist {
 
     Person[] public persons;
 
+    //--------------------------------------------------------------------- addPerson
     function addPerson(string memory _name, uint _age) public pure returns (Person memory) {
         Person memory p = Person(_name, _age);
         return p;
+    }
+
+    //--------------------------------------------------------------------- add
+    function add(string memory _name, uint _age) public {
+        persons.push(addPerson(_name, _age));
+    }
+
+    //--------------------------------------------------------------------- remove
+    function remove() public {
+        persons.pop();
     }
 }
