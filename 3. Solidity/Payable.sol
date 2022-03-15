5590a3892a1270588e249ae90ee992a353a94573
Gilles Bruno <gilles.bruno@bappli.com>
2022-03-15 11:54:52 +0100

    Payable training

---
+++ 3. Solidity/Payable.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+contract Payable {
+
+    address payable public owner;
+
+    constructor () {
+        owner = payable(msg.sender);
+    }
+
+    /**
+    * "payable" type enables to send some wei to this contract. 
+    * This smart contract can receive wei/eth
+    */
+    function deposit() external payable {
+
+    }
+
+    function getBalance() external view returns (uint)  {
+        return address(this).balance;
+    }
+}
