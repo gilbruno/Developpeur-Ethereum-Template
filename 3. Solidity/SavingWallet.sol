3e139f1f3b728b0a5e953baa1fc93c45cc82a033
Gilles Bruno <gilles_bruno@gmx.fr>
2022-03-16 12:40:16 +0100

    add new features

--- 3. Solidity/SavingWallet.sol
+++ 3. Solidity/SavingWallet.sol
@@ -7,10 +7,11 @@ contract SavingWallet {
     //------ STATE VARIABLES ------------------
     address public admin;
     string public name;
-    uint firstTx; //Timestamp de la 1ère tx
+    uint public creationTime = block.timestamp; //Timestamp de la 1ère tx
+    uint public currentIndexTx;
 
     //------ MAPPINGS ----------------------
-    mapping(uint => uint) historyOfTx;
+    mapping(uint => uint) public historyOfTx;
 
     //------ EVENTS ----------------------
     event Log(string msg, uint amount_received);
@@ -38,6 +39,29 @@ contract SavingWallet {
     receive() external payable{
         emit Log("Montant recu", msg.value);
     }
+
+    function history() public {
+
+    }
+
+    /** 
+    function addMonths(uint timestamp, uint _months) internal pure returns (uint newTimestamp) {
+        uint year;
+        uint month;
+        uint day;
+        (year, month, day) = _daysToDate(timestamp / SECONDS_PER_DAY);
+        month += _months;
+        year += (month - 1) / 12;
+        month = (month - 1) % 12 + 1;
+        uint daysInMonth = _getDaysInMonth(year, month);
+        if (day > daysInMonth) {
+            day = daysInMonth;
+        }
+        newTimestamp = _daysFromDate(year, month, day) * SECONDS_PER_DAY + timestamp % SECONDS_PER_DAY;
+        require(newTimestamp >= timestamp);
+    }
+     */
+    
 
     //--------------------------------------------------------------------- withdraw
     function withdraw(uint _amount) external onlyOwner onlyAfter3Months {
